c4f04f26ab0e6fdb3d03549e165a10b6a1577f03
ShishirSV <147775693+ShishirSV@users.noreply.github.com>
2024-04-25 11:51:31 +0530

    added js env

--- trial_plugins/ui-tests/jupyter_server_config.py
+++
@@ -1,7 +0,0 @@
-c.ServerApp.port = 8866
-c.ServerApp.open_browser = False
-c.ServerApp.token = ""
-c.ServerApp.password = ""
-c.ServerApp.disable_check_xsrf = True
-
-c.LabApp.expose_app_in_browser = True
